6c3052889a6b85c2c92ad9ec9eee239dac0e5e90
brince0304-payhere <seokhyun.beck@payhere.in>
2023-12-19 17:50:10 +0900

    feat: 서켠

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,8 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Learn Re
         </a>
       </header>
     </div>
